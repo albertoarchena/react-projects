959a350f266af0d76a571e01cc53da65877d7fcf
albertoarchena <albertoarchenaomg@gmail.com>
2023-08-01 03:15:38 +0200

    navbar 1.0

--- vite-portfolio/src/App.jsx
+++ vite-portfolio/src/App.jsx
@@ -5,8 +5,81 @@ function App() {
 
   return (
     <>
-      <main>
+      <main className="bg-[#1a232e]">
         <Navbar />
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente sint veniam nesciunt deserunt voluptatibus? Nihil, ducimus. Veritatis, fugiat nobis.</p>
       </main>
     </>
   )
--- vite-portfolio/src/components/Navbar.jsx
+++ vite-portfolio/src/components/Navbar.jsx
@@ -1,11 +1,35 @@
+import { useState, useEffect } from "react";
+
 const Navbar = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0){
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+  }, [])
+
+  // #0d1326
+
   return (
-    <div className="shadow-sm shadow-[#1d2336]">
-      <nav className="flex justify-between items-center h-24 max-w-[1240px] mx-auto bg-transparent">
+    <div className={`${isScrolled ? "sticky top-0 bg-[#1a232e] bg-opacity-80 backdrop-filter backdrop-blur-sm shadow-sm shadow-[#1d2336]" : "bg-transparent"} duration-1000 transition-all`}>
+      <nav className={`flex justify-between items-center max-w-[1240px] mx-auto ${ isScrolled ? "h-16" : "h-24"} transition-all duration-500`}>
         <div>
           <h1 className="w-full text-3xl font-bold text-cyan-300">{`{ Alberto Moreno }`}</h1>
         </div>
-        <div className="flex justify-between gap-20 items-center ">
+        <div className="flex justify-between gap-6 items-center ">
           <ul className="flex">
             <li className="p-4">
               <a className="hover:text-cyan-300" href="#">Projects</a></li>
