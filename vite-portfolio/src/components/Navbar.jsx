56896cedd6586cd15b165adac282240d8acce9f2
albertoarchena <albertoarchenaomg@gmail.com>
2023-08-01 12:07:33 +0200

    navbar 2.0 (responsive)

--- vite-portfolio/src/components/Navbar.jsx
+++ vite-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import { BiMenuAltRight, BiX } from 'react-icons/bi'
 
 const Navbar = () => {
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const [navResponsive, setNavResponsive] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,35 +23,66 @@ const Navbar = () => {
 
   }, [])
 
+  const handleClick = () => setNavResponsive(!navResponsive);
+
   // #0d1326
 
   return (
-    <div className={`${isScrolled ? "sticky top-0 bg-[#1a232e] bg-opacity-80 backdrop-filter backdrop-blur-sm shadow-sm shadow-[#1d2336]" : "bg-transparent"} duration-1000 transition-all`}>
-      <nav className={`flex justify-between items-center max-w-[1240px] mx-auto ${ isScrolled ? "h-16" : "h-24"} transition-all duration-500`}>
-        <div>
-          <h1 className="w-full text-3xl font-bold text-cyan-300">{`{ Alberto Moreno }`}</h1>
+    <div className={`${isScrolled ? "sticky top-0 bg-[#1a232e] bg-opacity-80 backdrop-filter backdrop-blur-sm" : "bg-transparent"} duration-500 transition-all`}>
+      <nav className={`flex justify-between items-center max-w-[1240px] lg:mx-auto mx-4  ${ isScrolled ? "h-16" : "h-24"} transition-all duration-500`}>
+        <div className="">
+          <a href="/">
+            <img 
+                src="/logo.svg"
+                alt="logo"
+                className={`cursor-pointer ${isScrolled ? "h-[40px] " : "h-[45px]  "} transition-all duration-500`}
+              />
+          </a> 
         </div>
-        <div className="flex justify-between gap-6 items-center ">
+        <div className="hidden md:flex justify-between gap-4 items-center ">
           <ul className="flex">
             <li className="p-4">
-              <a className="hover:text-cyan-300" href="#">Projects</a></li>
+              <a className="hover:text-white" href="/Projects">Projects</a></li>
             <li className="p-4">
-              <a className="hover:text-cyan-300" href="#">Experience</a>
+              <a className="hover:text-white" href="/Experiece">Experience</a>
             </li>
             <li className="p-4">
-              <a className="hover:text-cyan-300" href="#">Skills</a>
+              <a className="hover:text-white" href="/Skills">Skills</a>
             </li>
             <li className="p-4">
-              <a className="hover:text-cyan-300" href="#">About</a>
+              <a className="hover:text-white" href="/About">About</a>
             </li>
             <li className="p-4">
-              <a className="hover:text-cyan-300" href="#">Contact</a>
+              <a className="hover:text-white" href="/Contact">Contact</a>
             </li>
           </ul>
           <div>
-            <button className="bg-transparent hover:bg-[#1d2336] text-cyan-300 font-semibold py-2 px-4 border border-cyan-300 rounded-lg">Resume</button>
+            <button className="bg-transparent hover:bg-[#43455c] text-[#3bba9c] font-semibold py-2 px-4 border border-[#3bba9c] rounded-lg">Resume</button>
           </div>
         </div> 
+
+        {/* menu hamburguesa */}
+        <div onClick={handleClick} className="md:hidden z-10">
+          {!navResponsive ? <BiMenuAltRight size={30}/> : <BiX size={30}/>}
+        </div>
+
+        {/* menu responsive */}
+        <ul className={!navResponsive ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#1a232e] flex flex-col justify-center items-center"}>
+          <li className="py-8 text-4xl">
+            <a className="hover:text-white" href="/Projects">Projects</a></li>
+          <li className="py-8 text-4xl">
+            <a className="hover:text-white" href="/Experiece">Experience</a>
+          </li>
+          <li className="py-8 text-4xl">
+            <a className="hover:text-white" href="/Skills">Skills</a>
+          </li>
+          <li className="py-8 text-4xl">
+            <a className="hover:text-white" href="/About">About</a>
+          </li>
+          <li className="py-8 text-4xl">
+            <a className="hover:text-white" href="/Contact">Contact</a>
+          </li>
+        </ul>
       </nav>
     </div>
     
