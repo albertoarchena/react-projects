66f0dff12e5a0dd63fd33c88dbd5477da3564ccf
albertoarchena <albertoarchenaomg@gmail.com>
2023-08-02 00:18:53 +0200

    hero added

---
+++ vite-portfolio/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+
+const Hero = () => {
+  return (
+    <section className="max-w-[1240px] mx-auto my-[15rem] py-12">
+      <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-7xl">Alberto Moreno.</h1>
+      <h2 className="mt-3 text-xl font-bold tracking-tight text-slate-400 sm:text-7xl ">Software Engineer.</h2>
+      <p className="mt-10 max-w-sm leading-normal font-medium">
+        I&apos;m a software engineer specialized on full-stack development. Currently, I&apos;m working as a QA Engineer, developing automated tests at <a className="text-[#3bba9c]" href="https://www.capgemini.com/">Capgemini</a>.
+      </p>
+      <div className="flex gap-4 mt-8">
+        <button className="bg-[#3bba9c] hover:bg-[#0b8a6c] hover:text-white text-[#1a232e] font-semibold py-2 px-4 rounded-lg"><a href="#Projects">Projects</a></button>
+        <button className="bg-transparent hover:bg-[#2a333e] text-[#3bba9c] font-semibold py-2 px-4 border border-[#3bba9c] rounded-lg"><a href="">LinkedIn</a></button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
