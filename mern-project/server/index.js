00eca0cb630384c1f40f6f2acc28a65365dbeeb4
albertoarchena <albertoarchenaomg@gmail.com>
2023-08-03 10:57:04 +0200

    server post implementation

--- mern-project/server/index.js
+++ mern-project/server/index.js
@@ -1,10 +1,23 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
+app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.url({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://albertoarchena:albertoarchena123@cluster0.oixvgos.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, UseUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// https://www.mongodb.com/atlas/database
